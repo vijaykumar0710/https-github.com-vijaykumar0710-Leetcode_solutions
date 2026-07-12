62b91febcbddf548ff7d14aa99a04646a75d09e3
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-07-12 13:10:27 +0530

    Time: 23 ms (23.15%), Space: 14.9 MB (27.85%) - LeetHub

--- 3988-create-grid-with-exactly-k-paths-i/3988-create-grid-with-exactly-k-paths-i.cpp
+++ 3988-create-grid-with-exactly-k-paths-i/3988-create-grid-with-exactly-k-paths-i.cpp
@@ -1,9 +1,38 @@
 class Solution {
 public:
+    int a, b, K;
+    vector<string> grid;
+    vector<pair<int,int>> cells;
+    int countPaths() {
+        vector<vector<int>> dp(a, vector<int>(b, 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < a; i++) {
+            for(int j = 0; j < b; j++) {
+                if(grid[i][j] == '#') {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if(i == 0 && j == 0) continue;
+                if(i > 0) dp[i][j] += dp[i - 1][j];
+                if(j > 0) dp[i][j] += dp[i][j - 1];
+            }
+        }
+        return dp[a - 1][b - 1];
+    }
+    bool generate(int idx) {
+        if(idx == cells.size()) return countPaths() == K;
+        auto [i, j] = cells[idx];
+        grid[i][j] = '.';
+        if(generate(idx + 1)) return true;
+        grid[i][j] = '#';
+        if(generate(idx + 1)) return true;
+        return false;
+    }
     vector<string> createGrid(int m, int n, int k) {
-        int a = min(m, 4);
-        int b = min(n, 4);
-        vector<pair<int, int>> cells;
+        K = k;
+        a = min(m, 4);
+        b = min(n, 4);
+        grid.assign(a, string(b, '.'));
         for(int i = 0; i < a; i++) {
             for(int j = 0; j < b; j++) {
                 if(i == 0 && j == 0) continue;
@@ -11,41 +40,15 @@ public:
                 cells.push_back({i, j});
             }
         }
-        int sz = cells.size();
-        for(int mask = 0; mask < (1 << sz); mask++) {
-            vector<string> gadget(a, string(b, '.'));
-            for(int bit = 0; bit < sz; bit++) {
-                if(mask & (1 << bit)) {
-                    auto [i, j] = cells[bit];
-                    gadget[i][j] = '#';
-                }
+        if(!generate(0)) return {};
+        vector<string> ans(m, string(n, '#'));
+        for(int i = 0; i < a; i++) {
+            for(int j = 0; j < b; j++) {
+                ans[i][j] = grid[i][j];
             }
-            vector<vector<int>> dp(a, vector<int>(b, 0));
-            dp[0][0] = 1;
-            for(int i = 0; i < a; i++) {
-                for(int j = 0; j < b; j++) {
-                    if(gadget[i][j] == '#') {
-                        dp[i][j] = 0;
-                        continue;
-                    }
-                    if(i == 0 && j == 0) continue;
-                    if(i > 0) dp[i][j] += dp[i - 1][j];
-                    if(j > 0) dp[i][j] += dp[i][j - 1];
-                }
-            }
-            if(dp[a - 1][b - 1] != k) continue;
-            vector<string> ans(m, string(n, '#'));
-            for(int i = 0; i < a; i++) {
-                for(int j = 0; j < b; j++) {
-                    ans[i][j] = gadget[i][j];
-                }
-            }
-            for(int j = b; j < n; j++)
-                ans[a - 1][j] = '.';
-            for(int i = a; i < m; i++)
-                ans[i][n - 1] = '.';
-            return ans;
         }
-        return {};
+        for(int j = b; j < n; j++) ans[a - 1][j] = '.';
+        for(int i = a; i < m; i++) ans[i][n - 1] = '.';
+        return ans;
     }
 };
