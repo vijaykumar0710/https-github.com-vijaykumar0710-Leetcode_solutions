bb5c387d1dc2e3f1a6483c6fcccbf07e85394894
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-07-14 08:15:04 +0530

    Time: 1073 ms (50.43%), Space: 58.1 MB (80%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
 const int M=1e9+7;
 int t[201][201][201];
-int f(int i,vector<int>&nums,int gcd1,int gcd2,int n){
-   if(i>=n){
-    if(gcd1==gcd2) return 1;
-    return 0;
-   }
-   int res=0;
-   if(t[i][gcd1][gcd2]!=-1) return t[i][gcd1][gcd2];
-   res=(res+f(i+1,nums,gcd1,gcd2,n))%M;
-   res=(res+f(i+1,nums,gcd(gcd1,nums[i]),gcd2,n))%M;
-   res=(res+f(i+1,nums,gcd1,gcd(gcd2,nums[i]),n))%M;
-   return t[i][gcd1][gcd2]=res%M;
+int f(int i,int gcd1,int gcd2,vector<int>&nums,int n){
+    if(i>=n){
+        if(gcd1==gcd2) return 1;
+        return 0;
+    }
+    if(t[i][gcd1][gcd2]!=-1) return t[i][gcd1][gcd2];
+    int res=0;
+    res=(res+f(i+1,gcd1,gcd2,nums,n))%M;
+    res=(res+f(i+1,gcd(gcd1,nums[i]),gcd2,nums,n))%M;
+    res=(res+f(i+1,gcd1,gcd(gcd2,nums[i]),nums,n))%M;
+    return t[i][gcd1][gcd2]=res%M;
 }
     int subsequencePairCount(vector<int>& nums) {
         int n=nums.size();
         memset(t,-1,sizeof(t));
-        return f(0,nums,0,0,n)-1;
+        return f(0,0,0,nums,n)-1;
     }
 };
