45d7f7fa593d8f91607712a118f72a555866f63f
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-07-22 12:02:45 +0530

    Time: 1073 ms (36.39%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,8 @@
-SELECT e.name,b.bonus
+WITH cte AS(SELECT e.name,b.bonus
 FROM Employee e
 LEFT JOIN Bonus b
-ON e.empId=b.empId
-HAVING b.bonus<1000 || b.bonus IS NULL
+ON e.empId=b.empId)
+
+SELECT name,bonus
+FROM cte 
+HAVING bonus<1000 || bonus IS NULL
