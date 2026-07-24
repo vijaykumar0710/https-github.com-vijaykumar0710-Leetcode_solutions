15e9eb3d35b87c29a904547e33d4125b87abeaf9
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-07-24 08:52:50 +0530

    Time: 357 ms (57.56%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,11 +1,7 @@
 # Write your MySQL query statement below
-WITH cte AS(SELECT e1.id,e1.name,COUNT(e1.id) AS cnt
+SELECT e1.name as name
 FROM Employee e1
 JOIN Employee e2
 ON e1.id=e2.managerId
 GROUP BY e1.id
-)
-
-SELECT name 
-FROM cte
-WHERE cnt>=5;
+HAVING COUNT(e1.id)>=5
