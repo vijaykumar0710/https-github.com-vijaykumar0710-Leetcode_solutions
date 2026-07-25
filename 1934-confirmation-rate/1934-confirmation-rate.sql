68b7cffb7dd4110d316393e2c7e91374e527a37f
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-07-25 13:59:21 +0530

    Time: 691 ms (53.22%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-SELECT s.user_id,ROUND(SUM(IF(c.action='confirmed',1,0))/COUNT(*),2) AS confirmation_rate
+SELECT s.user_id,ROUND(SUM(IF(c.action='confirmed',1,0))/COUNT(s.user_id),2) AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c
 ON s.user_id=c.user_id
