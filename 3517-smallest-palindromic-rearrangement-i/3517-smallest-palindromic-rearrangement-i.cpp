f4e15b59425ac2b1967ce94ee85d8a3bab5a15d9
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-07-28 08:00:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -3,7 +3,7 @@ public:
     string smallestPalindrome(string s) {
         int n=s.size();
         sort(s.begin(),s.begin()+n/2);
-        sort(s.rbegin(),s.rbegin()+n/2);
+        sort(s.begin()+(n+1)/2,s.end(),greater<>());
         return s;
     }
 };
