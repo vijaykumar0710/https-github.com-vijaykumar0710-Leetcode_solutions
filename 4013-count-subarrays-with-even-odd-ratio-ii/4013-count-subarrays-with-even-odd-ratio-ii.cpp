55ac830d3bbba6cd824bd165cd7069bc35d59614
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-08-03 10:06:52 +0530

    Time: 2036 ms (12.5%), Space: 362 MB (25%) - LeetHub

--- 4013-count-subarrays-with-even-odd-ratio-ii/4013-count-subarrays-with-even-odd-ratio-ii.cpp
+++ 4013-count-subarrays-with-even-odd-ratio-ii/4013-count-subarrays-with-even-odd-ratio-ii.cpp
@@ -6,30 +6,30 @@ long long m;
 void update(long long i){
 for(;i<=m;i+=i&-i) BIT[i]+=1;
 }
-int query(long long i){
-long long res=0;
-for(;i>0;i-=(i&-i)) res+=BIT[i]; 
-  return res;
+long long query(long long i){
+    long long ans=0;
+    for(;i>0;i-=(i&-i)) res+=BIT[i];
+    return ans;
 }
-long long countInversion(vector<long long>& nums) {
-        int n=nums.size();
-        long long maxi=2e18+1;
-        m=2*maxi+1;
-        for(int i=n-1;i>=0;i--){
-            long long x=query(nums[i]+maxi);
-            res+=x;
-            update(nums[i]+maxi);
-        }
-        return res;
+long long countInversion(vector<long long>&pref){
+    int n=pref.size();
+    long long maxi=2e18+1;
+    m=2*maxi+1;
+    for(int i=n-1;i>=0;i--){
+        long long x=query(pref[i]+maxi);
+        res+=x;
+        update(pref[i]+maxi);
     }
+    return res;
+}
     long long countRatioSubarrays(vector<int>& nums, int a, int b) {
         int n=nums.size();
         for(int i=0;i<n;i++){
             if(nums[i]%2) nums[i]=-a;
             else nums[i]=b;
         }
-        vector<long long>pre(n+1,0);
-        for(int i=0;i<n;i++) pre[i+1]=pre[i]+nums[i];
-        return countInversion(pre);
+        vector<long long>pref(n+1,0);
+        for(int i=0;i<n;i++) pref[i+1]=pref[i]+nums[i];
+        return countInversion(pref);
     }
 };
