368ac0ba36dc69f7a6b1649e25f3a7af3ce2b312
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-08-09 11:30:58 +0530

    Time: 45 ms (85.14%), Space: 180.4 MB (80.52%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,33 +1,29 @@
 class NumArray {
 public:
-    vector<int> vec;
-    vector<int> BIT;
-    int n;
-    void updatefn(int i, int val) {
-        for (; i <= n; i += (i & -i))
-            BIT[i] += val;
-    }
-    int queryfn(int i) {
-        int sum = 0;
-        for (; i > 0; i -= (i & -i))
-            sum += BIT[i];
-        return sum;
-    }
-
+vector<int>vec,BIT;
+int n;
+void updatefn(int i,int val){
+    for(;i<=n;i+=(i&-i)) BIT[i]+=val;
+}
+int query(int i){
+    int sum=0;
+    for(;i>0;i-=(i&-i)) sum+=BIT[i];
+    return sum;
+}
     NumArray(vector<int>& nums) {
-        vec = nums;
-        n = nums.size();
-        BIT.assign(n + 1,0);
+        n=nums.size();
+        BIT.assign(n+1,0);
+        vec=nums;
         for(int i=0;i<n;i++) updatefn(i+1,nums[i]);
     }
-
+    
     void update(int index, int val) {
         updatefn(index+1,val-vec[index]);
         vec[index]=val;
     }
-
+    
     int sumRange(int left, int right) {
-        return queryfn(right+1)-queryfn(left);
+        return query(right+1)-query(left);
     }
 };
 
