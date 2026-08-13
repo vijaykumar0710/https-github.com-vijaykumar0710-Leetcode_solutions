353c2a6a2c01393c639852bd57e826b758baaca1
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-08-13 07:52:31 +0530

    Time: 67 ms (89.8%), Space: 118.1 MB (63.4%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-vector<int>res;
 vector<int>BIT;
-int m;
-void update(int i){
-for(;i<=m;i+=i&-i) BIT[i]+=1;
+int MAXN=1e5+5;
+void update(int i,int val){
+    for(;i<=MAXN;i+=i&-i) BIT[i]+=val;
 }
 int query(int i){
-int res=0;
-for(;i>0;i-=(i&-i)){
-    res+=BIT[i]; 
-  }
-  return res;
+    int cnt=0;
+    for(;i>=1;i-=i&-i) cnt+=BIT[i];
+    return cnt;
 }
     vector<int> countSmaller(vector<int>& nums) {
         int n=nums.size();
-        res.resize(n);
         BIT.resize(100005);
-        m=100005;
-       vector<int>copy;
-       copy=nums;
-       sort(copy.begin(),copy.end());
-       copy.erase(unique(copy.begin(),copy.end()),copy.end());
-       for(int i=0;i<n;i++){
-        int rank=lower_bound(copy.begin(),copy.end(),nums[i])-copy.begin();
-        nums[i]=rank+1;
-       }
+        vector<int>copy;
+        // Co-ordinate Compression
+        copy=nums;
+        sort(copy.begin(),copy.end());
+        copy.erase(unique(copy.begin(),copy.end()),copy.end());
+        for(int i=0;i<n;i++){
+            int rank=lower_bound(copy.begin(),copy.end(),nums[i])-copy.begin();
+            nums[i]=rank+1;
+        }
+        // Actual frequency related
+        vector<int>res(n);
         for(int i=n-1;i>=0;i--){
-            int x=query(nums[i]-1);
-            res[i]=x;
-            update(nums[i]);
+          int ans=query(nums[i]-1);
+          res[i]=ans;
+          update(nums[i],1);
         }
         return res;
     }
