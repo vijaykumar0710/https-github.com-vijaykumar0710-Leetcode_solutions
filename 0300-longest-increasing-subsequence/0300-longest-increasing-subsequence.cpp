442cb1c02fb7a4ef382c4e7c56cd6d5c2755730a
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-08-22 22:00:23 +0530

    Time: 6 ms (75.17%), Space: 14.6 MB (39.22%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
+int N;
+vector<int>BIT;
+void update(int i,int val){
+    for(;i<=N;i+=i&-i) BIT[i]=max(BIT[i],val);
+}
+int query(int i){
+    int len=0;
+    for(;i>0;i-=i&-i) len=max(len,BIT[i]);
+    return len;
+}
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> t(n, 1);
-        int lis = 1;
-        for (int i = 0; i < n; i++) {
-             int max_len = 0;
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    max_len = max(max_len, t[j]);
-                }
-            }
-            t[i] = 1 + max_len;
-            lis = max(lis, t[i]);
+        int n=nums.size();
+        vector<int>copy;
+        copy=nums;
+        sort(copy.begin(),copy.end());
+        copy.erase(unique(copy.begin(),copy.end()),copy.end());
+        for(int i=0;i<n;i++){
+            int rank=lower_bound(copy.begin(),copy.end(),nums[i])-copy.begin();
+            nums[i]=rank+1;
         }
-        return lis;
+        N=n;
+        BIT.assign(N+1,0);
+       int lis=1;
+       for(int i=0;i<n;i++){
+          int x=nums[i];
+          int len=1+query(x-1);
+          update(x,len);
+          lis=max(lis,len);
+       }
+       return lis;
     }
 };
