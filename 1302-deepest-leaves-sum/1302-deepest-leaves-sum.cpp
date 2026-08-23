145f47aa85ac22fab168505ad48811e0e085cbf5
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-08-24 00:42:18 +0530

    Time: 27 ms (5.76%), Space: 62.8 MB (19.75%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+priority_queue<pair<int,int>>pq;
+void fn(TreeNode* root,int d){
+    if(!root) return;
+    if(root->left) fn(root->left,d+1);
+    if(root->right) fn(root->right,d+1);
+    if(!root->left && !root->right){
+        pq.push({d,root->val});
+        return;
+    }
+}
+    int deepestLeavesSum(TreeNode* root) {
+        fn(root,0);
+        int sum=0;
+        int x=-1;
+        if(!pq.empty()) x=pq.top().first;
+        while(!pq.empty() && pq.top().first==x){
+            sum+=pq.top().second;
+            pq.pop();
+        }
+        return sum;
+    }
+};
