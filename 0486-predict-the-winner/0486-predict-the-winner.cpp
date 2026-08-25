079a64c0bd71efdf6434c2663bd903033199780f
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-08-25 13:03:27 +0530

    Time: 0 ms (100%), Space: 10.4 MB (5.82%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -3,11 +3,14 @@ public:
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>>t(n,vector<int>(n,0));
-        for(int len=1;len<=n;len++){
-            for(int i=0;len+i-1<n;i++){
+        for(int len=1;len<=n;len++){ 
+            for(int i=0;i<=n-len;i++){
                 int j=len+i-1;
-                if(len==1) t[i][i]=nums[i];
-                else t[i][j]=max(nums[i]-t[i+1][j],nums[j]-t[i][j-1]);
+                if(len==1){
+                    t[i][i]=nums[i];
+                    continue;
+                }
+               t[i][j]=max(nums[i]-t[i+1][j],nums[j]-t[i][j-1]);
             }
         }
         return t[0][n-1]>=0;
