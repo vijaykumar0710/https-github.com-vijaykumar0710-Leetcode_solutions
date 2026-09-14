07a74f5fa26bfea7ef3049c26c2b43d3b4ee2b6e
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-09-14 15:54:28 +0530

    Time: 358 ms (0%), Space: 15.1 MB (100%) - LeetHub

--- 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
+++ 4050-minimum-days-to-score-exactly-n-points/4050-minimum-days-to-score-exactly-n-points.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int t[100005];
-    int solve(long long sum) {
-        if (sum == 0) return 0;
-        if (t[sum] != -1) return t[sum];
-        int res = INT_MAX;
-        for (long long i = 1; (i * (i + 1)) / 2 <= sum; i++) {
-            long long T = (i * (i + 1)) / 2;
-            if (sum - T > 0) {
-                int sub = solve(sum - T);
-                if (sub != INT_MAX) {
-                    res = min(res, (int)(1 + i + sub));
-                }
-            } else if (sum - T == 0) {
-                res = min(res, (int)i); 
-            }
+int t[100001];
+int solve(long long sum){
+    if(sum==0) return 0;
+    if(t[sum]!=-1) return t[sum];
+   int res=INT_MAX;
+   for(int i=1;(i*(i+1))/2<=sum;i++){
+    long long x=(i*(i+1))/2;
+    if(sum-x>0){
+        int sub=solve(sum-x);
+        if(sub!=INT_MAX){
+            res=min(res,(i+1+sub));
         }
-        return t[sum] = res;
+    }else if(sum-x==0){
+        res=min(res,i);
     }
+   }
+   return t[sum]=res;
+}
     int minDays(int n) {
-        memset(t, -1, sizeof(t));
+        memset(t,-1,sizeof(t));
         return solve(n);
     }
 };
