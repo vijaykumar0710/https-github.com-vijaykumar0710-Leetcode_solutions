069f1b9a8c06284fb30c1bc05035076e5be6e1dc
Vijay Kumar <vijaykumar20cy@gmail.com>
2026-09-15 13:03:54 +0530

    Time: 223 ms (46.01%), Space: 162.2 MB (42.15%) - LeetHub

--- 4051-count-subarrays-with-distant-sums/4051-count-subarrays-with-distant-sums.cpp
+++ 4051-count-subarrays-with-distant-sums/4051-count-subarrays-with-distant-sums.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-long long MAXN;
+long long maxn;
 vector<long long>BIT;
 void update(int i,int val){
-    for(;i<=MAXN;i+=(i&-i)) BIT[i]+=val;
+for(;i<=maxn;i+=(i&-i)) BIT[i]+=val;
 }
 long long query(int i){
     long long res=0;
     for(;i>0;i-=(i&-i)) res+=BIT[i];
     return res;
 }
-long long countRangeSum(vector<int>&nums,int lower,int upper){
- int n=nums.size();
- if(lower>upper) return (1LL*n*(n+1))/2;
- vector<long long>pref(n+1,0);
- for(int i=0;i<n;i++) pref[i+1]=pref[i]+nums[i];
- vector<long long>temp=pref;
- for(auto p:pref){
-    temp.push_back(p-lower);
-    temp.push_back(p-upper);
- }
- sort(temp.begin(),temp.end());
- temp.erase(unique(temp.begin(),temp.end()),temp.end());
- MAXN=temp.size()+5;
- BIT.assign(MAXN+1,0);
- long long res=0;
- for(auto p:pref){
-    int r=lower_bound(temp.begin(),temp.end(),p-lower)-temp.begin()+1;
-    int l=lower_bound(temp.begin(),temp.end(),p-upper)-temp.begin()+1;
-    res+=query(r)-query(l-1);
-    int rank=lower_bound(temp.begin(),temp.end(),p)-temp.begin()+1;
-    update(rank,1);
- }
- return (1LL*n*(n+1))/2-res;
-}
+    long long countRangeSum(vector<int>& nums, int lower, int upper) {
+        int n=nums.size();
+        if(lower>upper) return (1LL*n*(n+1))/2;
+        vector<long long>pre(n+1,0);
+        for(int i=0;i<n;i++) pre[i+1]=pre[i]+nums[i];
+        vector<long long>temp=pre;
+        for(auto p:pre){
+            temp.push_back(p-lower);
+            temp.push_back(p-upper);
+        }
+        sort(temp.begin(),temp.end());
+        temp.erase(unique(temp.begin(),temp.end()),temp.end());
+        maxn=temp.size()+5;
+        BIT.assign(maxn+1,0);
+        long long res=0;
+        for(auto p:pre){
+            int up=lower_bound(temp.begin(),temp.end(),p-lower)-temp.begin()+1;
+            int lo=lower_bound(temp.begin(),temp.end(),p-upper)-temp.begin()+1;
+            int rank=lower_bound(temp.begin(),temp.end(),p)-temp.begin()+1;
+            res+=query(up)-query(lo-1);
+            update(rank,1);
+        }
+        return (1LL*n*(n+1))/2-res;
+    }
     long long distantSubarrays(vector<int>& nums, int goal, int k) {
-        int lo=goal-k;
-        int up=goal+k;
-        return countRangeSum(nums,lo+1,up-1);
+        int lo=goal-k+1;
+        int up=goal+k-1;
+        return countRangeSum(nums,lo,up);
     }
 };
